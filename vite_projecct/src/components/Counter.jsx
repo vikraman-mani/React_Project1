e3e4e2d9ca6a803efc7e9b2c05d32309fd11e3c3
vikraman-mani <vikraman02032001@gmail.com>
2025-04-02 15:22:35 +0530

    Conditional Rendering

--- vite_projecct/src/components/Counter.jsx
+++ vite_projecct/src/components/Counter.jsx
@@ -9,10 +9,32 @@ const Counter = () => {
       setCount((prevCount) => prevCount + 1); 
     }
 
+    let message;     // if conditions
+    if(count < 10) {
+        message = (
+            <p> Clicked {count} times </p>
+        )
+    }
+    else if (count < 20 ) {
+        message = (
+            <p> Clicked {count} times, Click more times </p>
+        )
+    }
+
   return (
     <div>
-        <h1> Counter App - {count} </h1>
+        <h1> Counter App  </h1>
         <button onClick={ HandleIncrease}>    Increase  </button>
+
+        { message }
+
+        {
+            count >= 10 ? <p> Count is greater than 10 </p> : <p> Count is less than 10 </p> // ternary operator
+        }
+        {
+            count >= 20 && <p> Count is greater than 20 </p> // logical operator
+        }
+
     </div>
   )
 }
