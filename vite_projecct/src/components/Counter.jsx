76e9e9c5192039eed85f2c582adab2b9b43df470
vikraman-mani <vikraman02032001@gmail.com>
2025-04-02 15:09:39 +0530

    Clear

--- vite_projecct/src/components/Counter.jsx
+++ vite_projecct/src/components/Counter.jsx
@@ -6,20 +6,12 @@ const Counter = () => {
 
     function HandleIncrease() {
       // setCount(count + 1);
-      setCount((prevCount) => prevCount + 1); // Functional update
-      setCount((prevCount) => { return prevCount + 1 } );
-      setCount((prevCount) => prevCount + 1);
+      setCount((prevCount) => prevCount + 1); 
     }
-
-    function returnSample () {
-        console.log("returnSample called")
-        return 100;
-    }
-    let [sample, setSample] = useState( () => { return returnSample() } );  // pass function in useState
 
   return (
     <div>
-        <h1> Counter App - {count} - {sample} </h1>
+        <h1> Counter App - {count} </h1>
         <button onClick={ HandleIncrease}>    Increase  </button>
     </div>
   )
