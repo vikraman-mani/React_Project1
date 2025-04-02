24e937df07936a9d4f224ad1b20f8b8b36a27d2f
vikraman-mani <vikraman02032001@gmail.com>
2025-04-02 16:58:45 +0530

    defaultProps 2

--- vite_projecct/src/components/ProductDetails.jsx
+++ vite_projecct/src/components/ProductDetails.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const ProductDetails = (props) => {
 
-    let {name, price} = props
+    let {name = "no name", price=0} = props
   return (
     <section>
             <h3>{name} - {price} </h3>
@@ -17,9 +17,4 @@ export default ProductDetails
 ProductDetails.proptype = {
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired
-}
-
-ProductDetails.defaultProps = {
-    name: "No Name",
-    price: 0
 }
