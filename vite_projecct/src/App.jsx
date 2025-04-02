e15be291a7cb731da669358c6d53233b8d75bafd
vikraman-mani <vikraman02032001@gmail.com>
2025-04-02 12:12:22 +0530

    Clear

--- vite_projecct/src/App.jsx
+++ vite_projecct/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import {Content} from './components/Content'
+import Content from './components/Content'
 
 function App() {
 
--- vite_projecct/src/components/Content.jsx
+++ vite_projecct/src/components/Content.jsx
@@ -1,23 +1,25 @@
 // React Arrow Function Component
 // rafc
 
-import React from 'react'
-
-export const Content = () => {
-  return (
-    <div>Content</div>
-  )
-}
-
-
-//rafce 
-
 // import React from 'react'
 
-// const Content = () => {
+// export const Content = () => {
 //   return (
 //     <div>Content</div>
 //   )
 // }
 
-// export default Content
+
+//rafce 
+
+import React from 'react'
+
+const Content = () => {
+  return (
+    <main> 
+        <h1> Main Content</h1>
+    </main>
+  )
+}
+
+export default Content
